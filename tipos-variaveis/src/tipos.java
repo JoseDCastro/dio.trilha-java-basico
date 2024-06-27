b4aa04686001d1b61b797b981e9a0c36851ddccb
Jose D Castro <j.dcastro.nt@gmail.com>
2024-06-27 11:29:45 -0300

    atualização

--- tipos-variaveis/src/tipos.java
+++ tipos-variaveis/src/tipos.java
@@ -1,5 +1,34 @@
+// Os oito tipos primitivos em Java são:
+// boolean, byte, char, short, int, long, float e double. - esses tipos não são considerados objetos, e portanto representam valores brutos.
+// Os tipos primitivos são usados para armazenar valores simples, como números, caracteres
+// e lógicos. Eles são usados para armazenar valores simples, como números  
+// e caracteres, e são usados para armazenar valores simples, como números e caracteres.
+// Eles são armazenados diretamente na pilha de memória.(Memory stack)
+
+// o tipo float é preciso colocar o F no final, pode ser maiúsculo ou minúsculo
+// o tipo long coloca-se o L no final
+
 public class tipos {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        // o short é um tipo de dado que armazena valores inteiros de 16
+        int numeroNormal = numeroCurto;
+        // o int é um tipo de dado que armazena valores inteiros de 32
+        short numeroCurto2 = (short) numeroNormal; // um short nesse caso não aceitaria receber o úmero normal devido a ser um int que comporta mais do que um short 
+        // poderia comportar,sendo assim precisamos especificar que o valor não receberá mais do que o que o short suportaria.Caso contrário apresentaria um erro.
+    }
+
+// Variáveis e constantes
+
+// As Constantes devem sempre ser declaradas em CAIXA ALTA
+// As constantes são valores que não podem ser alterados durante a execução do programa.
+
+    public static void varConst(String[] args) {
+        
     }
 }
+
+
+
